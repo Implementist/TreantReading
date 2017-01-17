743b5c5a9f842bb12be75b4bc7ede75799830424
Implementist <yuyuyu4993@qq.com>
2017-01-17 23:36:00 +0800

    Update 2 of Splash Activity

--- TreantReading/app/src/main/java/com/implementist/treantreading/LogoSplashActivity.java
+++ TreantReading/app/src/main/java/com/implementist/treantreading/LogoSplashActivity.java
@@ -2,7 +2,9 @@ package com.implementist.treantreading;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.implementist.treantreading.pref.AppPref;
 
@@ -15,6 +17,13 @@ public class LogoSplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // 透明导航栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
 
         //注意，这里没有setContentView,单纯只是用来跳转到相应的Activity
         //目的是减少首屏渲染
